d1b2f687aa59c3c9667859899c898056ab8138ff
Londeka Zikalala <londekazikalala@gmail.com>
2023-06-06 14:03:24 +0200

    test skeleton finished and mocha initialized

---
+++ test/wellnessPlate-test.js
@@ -0,0 +1,40 @@
+describe('The WellnessPlate function', function(){
+it('should check that the QR code has valid information to identifu the test', function(){
+
+})
+it('should verify that the QR code is valid valid', function(){
+    
+})
+})
+
+describe('The mental health Test', function(){
+    it('should initialize the mental health test', function(){
+
+    })
+
+     it('should record User answers  correctly ', function(){
+
+    })
+
+     it('should calculate the user score  correctly', function(){
+
+    })
+     it('should assign a user to a category based on the score', function(){
+
+    })
+
+     it('should print User results along with their category', function(){
+
+    })
+
+})
+
+describe('The Mental Health Diet', function(){
+     it('should allow a user to fill personalization ', function(){
+
+    })
+
+     it('should generate a mental health diet based on the User personalization and category score', function(){
+
+    })
+})
