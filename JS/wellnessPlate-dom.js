4ffa037149ca0f8079706a2b548ebc6edd31da02
Londeka Zikalala <londekazikalala@gmail.com>
2023-06-08 00:28:02 +0200

    href added to questions next button and DOM constants relating to the results.html added to DOM file

--- JS/wellnessPlate-dom.js
+++ JS/wellnessPlate-dom.js
@@ -1,5 +1,8 @@
 const takeButton = document.querySelector(".btn-hero");
-
+const nextBtton =  document.querySelector(".next");
+const theResult =  document.querySelector(".condition");
+const resultScanner = document.querySelector(".qr-code-results");
+const resultScannerBtn =  document.querySelector(".scanButton");
 
 // Take button should redirect User to a disclaimer screen with QR code 
 
