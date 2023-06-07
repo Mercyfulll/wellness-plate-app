fa7febb608a9e90cf1c594205777e75dfefc8c13
Londeka Zikalala <londekazikalala@gmail.com>
2023-06-08 00:04:22 +0200

    error messages functions and test skeletons added along with getUserAnswer, getTotalScore, stressRadar

--- JS/wellnessPlate.js
+++ JS/wellnessPlate.js
@@ -1,6 +1,6 @@
 function wellnessPlate(){
+    var theQRCode = "#" ;
 var test = {
-    qrCode:"#" ,
     questions:[
         "q1",
         "q2",
@@ -17,9 +17,15 @@ var test = {
         "q13",
         "q14",
         "q15"
+],
+answers:[
+    {'a': 3,
+    'b':2,
+    'c':1}
 ]
 }
 var hasTestInitialized = false;
+
 
 function validateName(name){
     const regex = /^[a-zA-Z ]+$/;
@@ -39,21 +45,69 @@ function validateEmail(email) {
 }
 
 function initializeTest(){
-    hasTestInitialized = true;
+   return hasTestInitialized = true;
 }
 
-    function totalScore(){
+function getUserAnswer(selectedAnswer){
+    if (selectedAnswer === test.answers[0].a) {
+        return 'a';
+      } else if (selectedAnswer === test.answers[0].b) {
+        return 'b';
+      } else if (selectedAnswer === test.answers[0].c) {
+        return 'c';
+      }
+    }
+
+function getTotalScore(test, userAnswers) {
+    if (test.questions.length === userAnswers.length) {
+      let score = 0;
+      for (let i = 0; i < test.questions.length; i++) {
+        const answer = userAnswers[i];
+  
+        if (answer === "a") {
+          score += test.answers[0].a;
+        } else if (answer === "b") {
+          score += test.answers[0].b;
+        } else if (answer === "c") {
+          score += test.answers[0].c;
+        }
+      }
+      return score;
+    }
+}
+    function stressRadar(score) {
+        if (score >= 1 && score <= 15) {
+          return 'You have low level stress and favorable mood';
+        } else if (score >= 16 && score <= 30) {
+          return 'You have moderate level stress';
+        } else if (score >= 31 && score <= 45) {
+          return 'You have high stress levels, please seek medical advise before undertaking the diet.';
+        }
+    }
+
+    function validationErrorMessages(){
 
     }
 
+    function testErrorMessages(){
+
+    }
+
+    function generalErrorMessages(){
+
+    }
 
 return{
 validateName,
 validateEmail,
+getUserAnswer,
 initializeTest,
-totalScore,
+getTotalScore,
+stressRadar,
+validationErrorMessages,
+testErrorMessages,
+generalErrorMessages
 
 
 }
-
 }
