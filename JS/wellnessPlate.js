8ef2753e04845dcdd6d6ffddc29c078615d69a72
Londeka Zikalala <londekazikalala@gmail.com>
2023-06-07 20:55:21 +0200

    Mental health test done

--- JS/wellnessPlate.js
+++ JS/wellnessPlate.js
@@ -1,18 +1,26 @@
 function wellnessPlate(){
 var test = {
-   
+    qrCode:"#" ,
     questions:[
-
-],
-qrCode: ;
+        "q1",
+        "q2",
+        "q3",
+        "q4",
+        "q5",
+        "q6",
+        "q7",
+        "q8",
+        "q9",
+        "q10",
+        "q11",
+        "q12",
+        "q13",
+        "q14",
+        "q15"
+]
 }
 
-function radarScore() {
-    const categoryIndex = Math.floor(test.score / 5);
-    const category = test.categories[categoryIndex];
-    console.log(`Your score: ${test.score}`);
-    console.log(`Category: ${category}`);
-  }
+
 return{
 
 }
